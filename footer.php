9410b60f62e3a212139d413d60f5e0edb356bbb2
faza <x1rpl2smkpp@gmail.com>
2023-07-02 20:51:05 +0700

    first commit

---
+++ footer.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+</head>
+
+<body>
+
+    <!-- Start Footer Section -->
+    <footer class="footer-section footer-bg section-top-gap-100">
+        <div class="footer-wrapper">
+            <!-- Start Footer Top -->
+            <div class="footer-top">
+                <div class="container">
+                    <div class="row mb-n6">
+
+                        <div class="col-lg-3 col-sm-6 mb-12">
+                            <!-- Start Footer Single Item -->
+                            <div class="footer-widget-single-item footer-widget-color--none" data-aos="fade-up" data-aos-delay="0">
+                                <!-- <h5 class="title">INFORMATION</h5> -->
+                                <img style="width: 150px; height: auto; margin-left: auto; margin-right: auto; display: block;" src="assets/images/logo/test.png" alt="">
+                                <br/>
+                            </div>
+                            <!-- End Footer Single Item -->
+                        </div>
+
+
+                        <div  style="text-align: center;" class="col-lg-3 col-sm-6 mb-6">
+                            <!-- Start Footer Single Item -->
+                            <div class="footer-widget-single-item footer-widget-color--none" data-aos="fade-up" data-aos-delay="400">
+                                <h5 class="title">TOP CATEGORIES</h5>
+                                <ul class="footer-nav">
+                                    
+                                    <li><a href="shop-grid-sidebar-left.php">Men's Fashion</a></li>
+                                    <li><a href="shop-grid-sidebar-left.php">Ladies Fashion</a></li>
+
+                                </ul>
+                            </div>
+                            <!-- End Footer Single Item -->
+                        </div>
+
+
+                        <div  style="text-align: center;" class="col-lg-3 col-sm-6 mb-6">
+                            <!-- Start Footer Single Item -->
+                            <div class="footer-widget-single-item footer-widget-color--none" data-aos="fade-up" data-aos-delay="600">
+                                <h5 class="title">CONTACT US</h5>
+                                <div class="footer-about">
+
+                                    <ul class="footer-nav">
+                                    <li><a href="shop-grid-sidebar-left.php">Facebook</a></li>
+                                    <li><a href="shop-grid-sidebar-left.php">WhatApp</a></li>
+                                    <li><a href="shop-grid-sidebar-left.php">Instagram</a></li>
+
+                                </ul>
+                                    
+                                </div>
+                            </div>
+                            <!-- End Footer Single Item -->
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- End Footer Top -->
+
+            <!-- Start Footer Bottom -->
+        </div>
+    </footer>
+    <!-- End Footer Section -->
+
+
+</body>
+
+</html>
